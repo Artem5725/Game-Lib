d7606e87c0b88b30e0ca926544e42b4a0c39eee4
Artem5725 <roadfromreversertowebdesigner@mail.ru>
2023-05-07 01:12:05 +0300

    fix: some comments and vars for test

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -4,16 +4,23 @@ import GamePage from './GamePage';
 import MainPage from './MainPage';
 import UserPage from './UserPage';
 
+// UserPage
 // TODO по должно из юрла при роутинге браться
 // TODO placeholder
 const blockName = 'Test block';
+
+// MainPage
+// TODO по должно из юрла при роутинге браться
+// TODO placeholder
+const gameId = 1234;
 
 // TODO роутинг здесь внутри page-content
 const Page: React.FC<any> = (_props) => {
   return (
     <div className="page">
       {/* <UserPage blockName={blockName}></UserPage> */}
-      <MainPage></MainPage>
+      {/* <MainPage></MainPage> */}
+      <GamePage gameId={gameId}></GamePage>
     </div>
   );
 };
